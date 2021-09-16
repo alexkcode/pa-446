6736317c96bd7d8d156c23e4569a94acc35295ef
alexkcode <alexiskwan.uiuc@gmail.com>
2021-09-16 13:28:23 -0500

    hw2

--- hw2/hw2_revised.R
+++ hw2/hw2_revised.R
@@ -21,8 +21,22 @@ SHOW YOUR WORK
 "
 
 #if you write any code for the problem, please include your code/work here
+library(tidyverse)
+salary_full = read_csv("pa446_chicago_full.csv")
 
+head(salary_full)
 
+salary = 
+  salary_full %>% 
+  mutate(first_name = str_extract(Name, ", .*") %>%
+           str_replace(pattern = ",\\s\\s", replacement = "") %>%
+           str_replace(pattern = " .$", replacement = ""),
+         middle_name = str_extract(Name, ", .*") %>%
+           str_extract(pattern = " [A-Z]$") %>%
+           str_replace(pattern = " ", replacement = ""),
+         last_name = str_extract(Name, ".*,") %>%
+           str_replace(pattern = ",", replacement = ""))
+  
 #---PROBLEM 2---
 "
 We need one salary data point regardless of whether somoene is a hourly or salaried employee.
@@ -33,14 +47,17 @@ Call this column annual_salary in your dataframe.
 "
 
 #if you write any code for the problem, please include your code/work here
-
+salary = salary %>%
+  mutate(annual_salary = case_when(`Salary or Hourly` == "Salary" ~ `Annual Salary`,
+                                   `Salary or Hourly` == "Hourly" ~ `Hourly Rate`))
 
 #---PROBLEM 3---
 "
 You have noticed that you are missing gender data. 
 Your task is to impute gender data for as many people in 
 the Chicago salaries data as possible.
-You are very scrappy and found the Social Service Administration's data (https://www.ssa.gov/OACT/babynames/limits.html)
+You are very scrappy and found the Social Service Administration's data 
+(https://www.ssa.gov/OACT/babynames/limits.html)
 which maps first names to genders for Illinois residents.
 The file is called IL.TXT and is available on Blackboard.
 Use this data to identify the gender of as many individuals
@@ -56,8 +73,17 @@ REMEMBER TO SHOW YOUR WORK
 
 
 #if you write any code for the problem, please include your code/work here
+gender_il = read_csv("IL.TXT", 
+                     col_names = c("state","gender","year","first_name","count")) %>% 
+  mutate(first_name = str_to_upper(first_name)) %>%
+  group_by(first_name) %>%
+  filter(count == max(count)) %>%
+  filter(row_number(first_name) == 1)
 
+gender_salary = left_join(salary, gender_il) %>% select(gender)
 
+salary = salary %>%
+  mutate(new_gender = gender_salary)
 
 #---PROBLEM 4---
 "
@@ -68,3 +94,5 @@ JUST FIND THE DATA SOURCES ONLINE AND SHARE THE URL.
 "
 
 #if you write any code for the problem, please include your code/work here
+# https://www.bls.gov/sae/
+# https://www.census.gov/data/tables/time-series/demo/income-poverty/cps-hinc/hinc-01.html
